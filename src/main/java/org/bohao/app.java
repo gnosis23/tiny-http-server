4d0d3657fde5554a3134380d8e10895af3f14db8
gnosis23 <bj050323@qq.com>
2015-03-27 22:38:47 +0800

    init files

---
+++ src/main/java/org/bohao/app.java
@@ -0,0 +1,10 @@
+package org.bohao;
+
+/**
+ * Created by bohao on 03-27-0027.
+ */
+public class app {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
