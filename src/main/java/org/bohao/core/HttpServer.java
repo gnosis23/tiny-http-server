253570ce4956a05b08c517a030ea946d4e4084aa
gnosis23 <bj050323@qq.com>
2015-04-18 11:54:46 +0800

    改成多线程

--- src/main/java/org/bohao/core/HttpServer.java
+++ src/main/java/org/bohao/core/HttpServer.java
@@ -11,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by bohao on 03-28-0028.
@@ -24,14 +26,39 @@ public class HttpServer {
         serverSocket = new ServerSocket(port);
     }
 
-    public void start() {
-        while (true) {
-            try (
-                    Socket clientSocket = serverSocket.accept();
-                    HttpReader in = new HttpReader(clientSocket.getInputStream());
-                    HttpWriter out = new HttpWriter(clientSocket.getOutputStream())
-            ) {
+    public void start() throws IOException {
+        ExecutorService executor = Executors.newCachedThreadPool();
 
+        try {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+
+                Task task = new Task(clientSocket);
+                executor.execute(task);
+            }
+        } catch (Exception e) {
+            // here when disconnect
+            logger.info("Exception caught when trying to listen on port "
+                    + serverSocket.getInetAddress()
+                    + " or listening for a connection");
+            logger.info(e.getMessage());
+        } finally {
+            serverSocket.close();
+        }
+    }
+
+    private class Task implements Runnable {
+        public Socket clientSocket;
+
+        public Task(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try (HttpReader in = new HttpReader(clientSocket.getInputStream());
+                 HttpWriter out = new HttpWriter(clientSocket.getOutputStream())
+            ) {
                 HttpRequest request = in.getRequest();
                 HttpResponse response = new HttpResponse();
 
@@ -39,7 +66,6 @@ public class HttpServer {
                 resolver.process(request, response);
 
                 out.sendResponse(response);
-
             } catch (Exception e) {
                 // here when disconnect
                 logger.info("Exception caught when trying to listen on port "
