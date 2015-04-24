e6ad98527db86fbf7c9373589001b8daf998180e
gnosis23 <bj050323@qq.com>
2015-04-24 23:35:21 +0800

    线程安全性；

--- src/main/java/org/bohao/proto/HttpSession.java
+++ src/main/java/org/bohao/proto/HttpSession.java
@@ -10,23 +10,23 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class HttpSession {
     private Map<String, Object> attrs = new ConcurrentHashMap<>();
-    private long createTime;
-    private String sid;
+    private final long createTime;
+    private final String sid;
 
     public HttpSession(String no) {
         createTime = System.currentTimeMillis();
         sid = no;
     }
 
-    public Object getAttribute(String name) {
+    public synchronized Object getAttribute(String name) {
         return attrs.get(name);
     }
 
-    public void removeAttribute(String name) {
+    public synchronized void removeAttribute(String name) {
         attrs.remove(name);
     }
 
-    public void setAttribute(String name, Object value) {
+    public synchronized void setAttribute(String name, Object value) {
         attrs.put(name, value);
     }
 
@@ -38,7 +38,7 @@ public class HttpSession {
         return sid;
     }
 
-    public Enumeration<String> getAttributeNames() {
+    public synchronized Enumeration<String> getAttributeNames() {
         return Collections.enumeration(attrs.keySet());
     }
 }
